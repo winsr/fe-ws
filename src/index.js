06f47bb15c8c0171542a1dbe58ec529568836173
Endy <7ofpentacles@gmail.com>
2021-09-26 11:43:41 +0700

    recheck code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { createStore } from "redux"
-import { Provider } from "react-redux"
 
 import App from "./App"
-import rootReducers from "./store/reducer/index"
-
-// const store = createStore(rootReducers)
 
 ReactDOM.render(
-  // <Provider store={store}>
   <App />,
-  // </Provider>,
   document.getElementById("root")
 )
